1b684eba0698640ff00ee7f4db48c4927bc1759c
yoshitaka-tanaka0522 <66899297+yoshitaka-tanaka0522@users.noreply.github.com>
2021-02-21 17:37:23 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,46 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const nodemailer = require('nodemailer');
-const cors = require('cors')({origin: true});
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-
-admin.initializeApp();
-
-// Using Gmail
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
+var Mailer = require('nodemailer');
+var Smtpsetting = {
+  service: 'Gmail',
+  secure: true,
   auth: {
-    user: 'newlifetanaka', // here use your real email
-    pass: 'newlifetanaka2020' // put your password correctly (not in this question please)
+    user: 'test',
+    pass: 'test',
+    port: '465' 
   }
-});
+};
 
-// Sending the request
 exports.sendMail = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
+var Mailobject = {
+  from: 'req.body.mail',
+  to: 'test@gmail.com',
+  subject: 'This mail is test.',
+  text: `test`,
+  html: `
+  <html>
+  <head>
+  </head>
+  <body>
+  <h1>テスト送信</h1>
+  <p>テスト</p>
+  </body>
+  </html>
+  ` 
+};
 
-        // Getting query parameter from http request
-        const to = req.query.to;
-        const msg = req.query.msg;
-        const email = req.query.email;
-        const title = req.query.title;
+//SMTP
+var Smtp = Mailer.createTransport('SMTP', Smtpsetting);
 
-        const mailOptions = {
-            from: gmailEmail,
-            to: to,
-            subject: 'This is a sample of email function',
-            html: `${email}+${title}+${msg}`
-        };
+let Send = () => Smtp.sendMail(Mailobject, function (err, res) {
 
-        // Getting results
-        return transporter.sendMail(mailOptions, (erro, info) => {
-            if(erro){
-                return res.send(erro.toString());
-            }
-            return res.send('Sended');
-        });
-    });
+  if (err) {
+    console.log('エラー出力' + err);
+
+  } else {
+    console.log('テストメッセージ' + res.message);
+  }
+  //SMTP
+  Smtp.close();
+});
+Send();
+next();
 });
